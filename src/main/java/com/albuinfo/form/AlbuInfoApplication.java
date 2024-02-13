e58920f4a3a7335a829382fb36342c8d08b5f58b
christopherBryanSMC <mancillabryan1996@outlook.com>
2024-02-13 08:47:01 -0600

    estructura principal

---
+++ src/main/java/com/albuinfo/form/AlbuInfoApplication.java
@@ -0,0 +1,13 @@
+package com.albuinfo.form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbuInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbuInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/albuinfo/form/AlbuInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albuinfo.form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbuInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
